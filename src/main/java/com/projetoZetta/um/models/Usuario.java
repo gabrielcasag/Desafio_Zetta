1096e474f22fe41cb44b1ce59511609b11fef768
Gabriel Casagrande <gaspericasag@gmail.com>
2020-05-24 18:19:56 -0300

    Iniciado mapeamento de Usuario

--- src/main/java/com/projetoZetta/um/models/Usuario.java
+++ src/main/java/com/projetoZetta/um/models/Usuario.java
@@ -1,6 +1,28 @@
 package com.projetoZetta.um.models;
 
-public class Usuario {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
 
 	private String nome;
 	private String cpf;
