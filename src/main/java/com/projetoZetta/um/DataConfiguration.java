360351b5dbbe7f8b34e1c6627fa9ff9d1eefdbfe
Gabriel Casagrande <gaspericasag@gmail.com>
2020-05-24 17:49:19 -0300

    Iniciado configuracao do banco de dados

---
+++ src/main/java/com/projetoZetta/um/DataConfiguration.java
@@ -0,0 +1,17 @@
+package com.projetoZetta.um;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DataConfiguration {
+
+
+
+}
