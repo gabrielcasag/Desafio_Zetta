54b1ab7013d3477f4a946d486c3d180d8baf59a1
Gabriel Casagrande <gaspericasag@gmail.com>
2020-05-24 17:04:55 -0300

    Testando servidor

---
+++ src/main/java/com/projetoZetta/um/indexController.java
@@ -0,0 +1,14 @@
+package com.projetoZetta.um;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+
+}
