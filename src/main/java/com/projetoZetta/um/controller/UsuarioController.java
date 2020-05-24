7655799bcf5d29bd5e534e5671fd9ea07942a660
Gabriel Casagrande <gaspericasag@gmail.com>
2020-05-24 18:24:36 -0300

    Iniciado configuracoes da camada Repository

--- src/main/java/com/projetoZetta/um/controller/UsuarioController.java
+++ src/main/java/com/projetoZetta/um/controller/UsuarioController.java
@@ -1,13 +1,29 @@
 package com.projetoZetta.um.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.projetoZetta.um.models.Usuario;
+import com.projetoZetta.um.repository.UsuarioRepository;
 
 @Controller
 public class UsuarioController {
 	
-	@RequestMapping("/cadastrarUsuario")
-	public String form() {
+	@Autowired
+	private UsuarioRepository ur;
+	
+	@RequestMapping(value="/cadastrarUsuario", method=RequestMethod.GET)
+	public String form() {	
 		return "/usuario/formUsuario";
+	}
+	
+	@RequestMapping(value="/cadastrarUsuario", method=RequestMethod.POST)
+	public String form(Usuario usuario) {
+		
+		ur.save(usuario);
+		
+		return "redirect:/cadastrarUsuario";
 	}
 }
---
+++ src/main/java/com/projetoZetta/um/repository/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package com.projetoZetta.um.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import com.projetoZetta.um.models.Usuario;
+
+public interface UsuarioRepository extends CrudRepository<Usuario,String>{
+
+}
