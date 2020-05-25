8f0761350133c26b211823a18b0253041e769c27
Gabriel Casagrande <gaspericasag@gmail.com>
2020-05-24 21:31:09 -0300

    Adicionado classe Perfil

---
+++ src/main/java/com/projetoZetta/models/Perfil.java
@@ -0,0 +1,28 @@
+package com.projetoZetta.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(schema="usermanager" , name="perfil")
+public class Perfil {
+    
+	@Id
+    @Column(name = "id")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+	
+	@Column(name="nome")
+	private String nome;
+	
+    public Perfil() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
