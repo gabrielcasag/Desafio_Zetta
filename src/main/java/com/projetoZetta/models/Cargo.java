db444f68a6ba35f30a129d1badd6ac483818e358
Gabriel Casagrande <gaspericasag@gmail.com>
2020-05-24 21:33:45 -0300

    Adicionado classe Cargo

---
+++ src/main/java/com/projetoZetta/models/Cargo.java
@@ -0,0 +1,35 @@
+package com.projetoZetta.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(schema="usermanager" , name="cargo")
+public class Cargo {
+	
+	@Id
+	@Column(name = "id")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+
+	@Column(name="nome")
+	private String nome;
+	
+    public Cargo() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
