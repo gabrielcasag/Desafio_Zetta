a6a5b5c75a70381e4c9ad42680baa56c01a955de
Gabriel Casagrande <gaspericasag@gmail.com>
2020-05-24 23:07:17 -0300

    Adicionado o pacote dto ao projeto

--- src/main/java/com/projetoZetta/controller/UsuarioController.java
+++ src/main/java/com/projetoZetta/controller/UsuarioController.java
@@ -1,18 +1,38 @@
 package com.projetoZetta.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.projetoZetta.dto.MessageResponseDTO;
 import com.projetoZetta.models.Usuario;
 import com.projetoZetta.repository.UsuarioRepository;
 
-@CrossOrigin
-@Controller
+@RestController
+@RequestMapping("/api/v1/usuarios")
 public class UsuarioController {
 	
+	private UsuarioRepository repo;
+	
+	@Autowired
+	public UsuarioController(UsuarioRepository repo){
+		this.repo = repo;
+	}
+
+	@PostMapping
+	public MessageResponseDTO create(@RequestBody Usuario usuario) {
+		Usuario usuarioSalvo = repo.save(usuario);
+		return MessageResponseDTO.builder().message("Usuario criado com ID:" + usuarioSalvo.getId()).build();
+	}
+	
+	
+	
+	
+	
+	
+	
+	/*
 	@Autowired
 	private UsuarioRepository repo;
 	
@@ -25,5 +45,5 @@ public class UsuarioController {
 	public String form(Usuario usuario) {
 		repo.save(usuario);
 		return "redirect:/cadastrarUsuario";
-	}
+	}*/
 }
--- src/main/java/com/projetoZetta/repository/UsuarioRepository.java
+++ src/main/java/com/projetoZetta/repository/UsuarioRepository.java
@@ -1,11 +1,8 @@
 package com.projetoZetta.repository;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.projetoZetta.models.Usuario;
 
-@Repository
-public interface UsuarioRepository extends CrudRepository<Usuario,Long>{
+public interface UsuarioRepository extends JpaRepository<Usuario,Long>{
 
 }
